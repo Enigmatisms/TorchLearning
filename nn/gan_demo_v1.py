f13422ee6fc733f9bb950c5b73f74169faf6b972
Enigmatisms <984041003@qq.com>
2021-01-26 02:11:12 +0800

    GAN v2 not optimized yet operational.

--- nn/gan_demo_v1.py
+++ nn/gan_demo_v1.py
@@ -11,16 +11,14 @@ from gan_v1 import Gen
 
 if __name__ == "__main__":
     gan = torch.load("..\\models\\gan_gen.pkl")
-    cnt = 0
-    cnt += 1
-    x = torch.autograd.Variable(torch.randn(40, 256)).cuda()
+    x = torch.autograd.Variable(torch.randn(50, 128)).cuda()
     output = gan(x).cpu()
     print(output.shape)
-    for i in range(4):
+    for i in range(5):
         for j in range(10):
-            plt.subplot(4, 10, i * 10 + j + 1)
-            img = output[i * 10 + j].data.numpy()[0]
-            img = (img * 255).astype(np.uint8)
+            plt.subplot(5, 10, i * 10 + j + 1)
+            img = output[i * 10 + j].data[0]
+            img = img.clamp(0, 1).cpu()
             plt.imshow(img, cmap = 'gray')
     plt.show()
 
--- nn/gan_v1.py
+++ nn/gan_v1.py
@@ -1,13 +1,8 @@
 #-*-coding:utf-8-*-
 """
     生成对抗网络实践
-    由于自动编码器，比如VAE，是输入一个编码，输出对应编码的生成图片
-    而VAE比普通编码器的优势在于，其有随机性
-    那么如何才能知道自己生成的数据足够好呢？
-    再构建一个二分类网络，用于判定数据是真实的还是生成的，通过此网络来指导编码器的优化
-        使用MNIST进行手写数字风格生成（但是可能很难知道哪些是生成的，哪些是真实的）
-    version 1 多层感知机作为网络生成器
-    TODO: 有个奇怪的问题 batch 训练时的channels设置问题
+    第一版本完全死亡，生成出来的就不是数字，就是噪声，很漂亮的雪花噪声
+        GAN v1：雪花噪声生成网络
 """
 
 import torch
@@ -21,7 +16,6 @@ from torchvision import transforms
 class Disc(nn.Module):
     """
         二分类判别器
-        二分类判别器需要使用什么样的激活函数，网络结构需要是什么样的？
     """
     def __init__(self):
         super().__init__()
@@ -52,7 +46,6 @@ class Gen(nn.Module):
     """
         图片向量生成器
         输入一个in_sz大小的一维正态分布随机数向量
-        多层感知机生成
         输出一个大小为28 * 28的二维矩阵
     """
     def __init__(self, in_sz = 128, out_sz = 28):
@@ -61,7 +54,6 @@ class Gen(nn.Module):
             *Gen.block(in_size, 1024, False),
             *Gen.block(1024, 4096)
         )
-        # linMap之后，view为 64 * 64
         self.downConv1 = nn.Sequential(
             nn.Conv2d(1, 1, 3, 2, 1),
             nn.BatchNorm2d(1),
@@ -87,10 +79,8 @@ class Gen(nn.Module):
         return bloc
     
     def forward(self, x):
-        # print(x.shape)
         x = self.linMap(x)
         x = x.view(x.size(0), 1, 64, 64)
-        # print(x.shape)
         x1 = self.downConv1(x)
         x2 = self.downConv2(x)
         x = x1 + x2
@@ -101,16 +91,16 @@ if __name__ == "__main__":
     if torch.cuda.is_available() == False:
         raise RuntimeError("Cuda is not available.")
     tf = transforms.ToTensor()
-    batch_sz = 40               # batch大小
-    in_size = 256              # 生成网络的输入向量大小
-    train_iter = 12              # 训练迭代次数
+    batch_sz = 50                    # batch大小
+    in_size = 128                   # 生成网络的输入向量大小
+    train_iter = 12                 # 生成器训练迭代次数
     origin = datasets.MNIST("..\\data\\", True, transform = tf)
     origin_set = DataLoader(origin, batch_sz, shuffle = True)
     disc = Disc()
     generator = Gen(in_size, out_sz = 28)
     disc.cuda()
     generator.cuda()
-    dopt = optim.Adam(disc.parameters(), lr = 1e-4)
+    dopt = optim.Adam(disc.parameters(), lr = 2e-5)
     gopt = optim.Adam(generator.parameters(), lr = 2e-2)
     loss_func = nn.BCELoss()
     real_labels = Var(torch.ones((batch_sz, 1))).cuda()
@@ -119,25 +109,24 @@ if __name__ == "__main__":
         bx = bx.cuda()
         gen_loss = 0
         dis_loss = 0
-        out = disc(bx)                                  # 分类器输出
-        loss = loss_func(out, real_labels)     # 真实数据loss计算
+        out = disc(bx)
+        loss = loss_func(out, real_labels)
         batch_sz = len(bx)
-        false_batch = Var(torch.randn(batch_sz, in_size)).cuda()    # 生成batch_size个复合标准正态分布的向量
-        # 判别器训练 (清楚地让分类器知道什么是真的，什么是假的)
-        false_in = generator(false_batch)               # 使用生成器进行生成判别
-        false_out = disc(false_in)                      # 生成数据判别
-        # print("Shape of false in:", false_in.shape, ", shape of false out:", false_out.shape)
-        loss += loss_func(false_out, fake_labels)   # 综合两方面的loss
+        false_batch = Var(torch.randn(batch_sz, in_size)).cuda()
+        # ================= 判别器训练 =====================
+        false_in = generator(false_batch)
+        false_out = disc(false_in)
+        loss += loss_func(false_out, fake_labels)
         dis_loss = loss.data.item()
         dopt.zero_grad()
         loss.backward()
         dopt.step()
-        for i in range(train_iter):                         # 对每一个batch都进行train_iter次训练
-            # 生成器训练（生成器需要让分类器的结果尽可能为1）\
+        # ================= 生成器训练 =====================
+        for i in range(train_iter):
             false_batch = Var(torch.randn(batch_sz, in_size)).cuda()
             false_in = generator(false_batch)
-            false_out = disc(false_in)          # 根据训练后的生成器重新计算真假分类结果
-            loss = loss_func(false_out, real_labels) # 1是真实数据，生成网络需要调整至尽可能让分类器混淆
+            false_out = disc(false_in)
+            loss = loss_func(false_out, real_labels)
             if i ==  train_iter - 1:
                 gen_loss = loss.data.item()
             gopt.zero_grad()
